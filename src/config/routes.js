7892daaf382fed005c698593df351c03259e5e32
Arda Nakisci <anakisci@gmail.com>
2019-07-12 16:48:41 +0300

    add example screen

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
+import { Example as ExampleScreen } from './containers'
 
 const RouterComp = () => {
     return (
         <Router>
-            <Scene />
+            <Scene key="root" hideNavBar={true}>
+                <Scene key="example" component={ExampleScreen} />
+            </Scene>
         </Router>
     )
 }
