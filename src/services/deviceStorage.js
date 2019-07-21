10992246ba5b5d1aeb0d6dcfc3816a4830ecbf97
Arda Nakisci <anakisci@gmail.com>
2019-07-21 12:23:37 +0300

    add asyncStorage

---
+++ src/services/deviceStorage.js
@@ -0,0 +1,25 @@
+import { AsyncStorage } from 'react-native'
+
+export const deviceStorage = {
+    saveItemToStorage: saveItemToStorage,
+    getItemFromStorage: getItemFromStorage
+}
+
+const saveItemToStorage = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value)
+    } catch (error) {
+        // TODO Change console.log to something else
+        console.log('AsyncStorage Error: ' + error.message)
+    }
+}
+
+const getItemFromStorage = async key => {
+    try {
+        const item = await AsyncStorage.getItem(key)
+        return item
+    } catch (error) {
+        // TODO Change console.log to something else
+        console.log('AsyncStorage Error: ' + error.message)
+    }
+}
