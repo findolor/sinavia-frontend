3a612d240f16528f7f95f7e37105f43e108ef94e
haqo <hakanyilmazz95@gmail.com>
2019-07-22 15:11:53 +0300

    add bottombar and card

--- src/containers/mainScreens/homeScreen/home.js
+++ src/containers/mainScreens/homeScreen/home.js
@@ -1,11 +1,82 @@
 import React from 'react'
-import { Animated, Keyboard, Platform, StatusBar, Text, View } from 'react-native'
-import { heightPercentageToDP as hp } from "react-native-responsive-screen"
+import {
+    Animated,
+    Keyboard,
+    Platform,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+import {
+    heightPercentageToDP as hp,
+    widthPercentageToDP as wp
+} from 'react-native-responsive-screen'
+import { AuthButton, AuthTextInput } from '../../../components/authScreen'
 
 export default class Home extends React.Component {
     render() {
         return (
-            <Text>ANA  MENU</Text>
+            <View style={styles.container}>
+                <View style={{ height: hp(60), marginTop: hp(30) }}>
+                    <ScrollView style={styles.cardsScrollView}>
+                        <View style={styles.card}>
+                            <Text style={styles.cardText}>
+                                Paragrafta Anlam
+                            </Text>
+                        </View>
+                        <View style={styles.card} />
+                        <View style={styles.card} />
+                        <View style={styles.card} />
+                        <View style={styles.card} />
+                        <View style={styles.card} />
+                        <View style={styles.card} />
+                        <View style={styles.card} />
+                    </ScrollView>
+                </View>
+                <View style={styles.bottomBar} />
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#fcfcfc'
+    },
+    bottomBar: {
+        height: hp(9),
+        width: wp(100),
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
+        backgroundColor: '#00D9EF',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        marginTop: hp(88),
+        position: 'absolute'
+    },
+    cardsScrollView: {
+        height: hp(70),
+        width: wp(87),
+        backgroundColor: '#fcfcfc'
+    },
+    card: {
+        height: hp(9),
+        width: wp(87),
+        borderWidth: wp(0.6),
+        borderRadius: 10,
+        borderColor: '#00D9EF',
+        marginBottom: hp(2.3),
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    cardText: {
+        fontFamily: 'Averta-Regular',
+        color: '#00D9EF',
+        fontSize: hp(3)
+    }
+})
