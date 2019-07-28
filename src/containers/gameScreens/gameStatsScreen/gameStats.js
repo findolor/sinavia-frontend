0b37e9872393dd4b1a8fb47d2910aa6c3e575226
Arda Nakisci <anakisci@gmail.com>
2019-07-28 11:36:05 +0300

    add dynamic variables

--- src/containers/gameScreens/gameStatsScreen/gameStats.js
+++ src/containers/gameScreens/gameStatsScreen/gameStats.js
@@ -2,17 +2,47 @@ import React from 'react'
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import styles from './style'
 import background from '../../../assets/gameScreens/gameStatsBackground.jpg'
-import win from '../../../assets/gameScreens/win.png'
 import slideUp from '../../../assets/gameScreens/slideUp.png'
 import correct from '../../../assets/gameScreens/correct.png'
 import incorrect from '../../../assets/gameScreens/incorrect.png'
 import unanswered from '../../../assets/gameScreens/unanswered.png'
 import profilePic from '../../../assets/profile2.jpg'
 
+const YOU_WIN_LOGO = require('../../../assets/gameScreens/win.png')
+
 class GameStatsScreen extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            // If we are the winner, isClientWinner is true
+            isClientWinner: true,
+            // Client match results
+            correctAnswerNumber: 0,
+            incorrectAnswerNumber: 0,
+            unansweredAnswerNumber: 0,
+            // Opponent match results
+            opponentCorrectAnswerNumber: 0,
+            opponentInorrectAnswerNumber: 0,
+            opponentUnansweredAnswerNumber: 0,
+            // Opponent username
+            clientUsername: '',
+            opponentUsername: '',
+            // Match point variables
+            finishedGamePoint: 20,
+            correctAnswerPoint: 60,
+            winOrLosePoint: 100,
+            // Match win or lose text
+            winOrLoseText: 'Kazandın',
+            // Total earned points
+            totalEarnedPoints: 180,
+            // Player profile pictures
+            clientProfilePicture: '',
+            opponentProfilePicture: ''
+        }
+    }
+
+    componentDidMount() {
+        // TODO logic for filling the screen with results
     }
 
     render() {
@@ -24,7 +54,10 @@ class GameStatsScreen extends React.Component {
                 <View style={styles.container}>
                     <Image source={background} style={styles.background} />
                     <View style={styles.resultTextContainer}>
-                        <Image source={win} style={styles.resultTextImg} />
+                        <Image
+                            source={YOU_WIN_LOGO}
+                            style={styles.resultTextImg}
+                        />
                     </View>
                     <View style={styles.resultsContainer}>
                         <View style={styles.results1Container}>
@@ -34,33 +67,51 @@ class GameStatsScreen extends React.Component {
                                     style={styles.profilePic}
                                 />
                                 <Text style={styles.usernameText}>
-                                    haqotherage
+                                    {this.state.clientUsername}
                                 </Text>
                             </View>
                             <View style={styles.answersContainer}>
                                 <View style={styles.dividedAnswer}>
-                                    <Text style={styles.numbers}>1</Text>
+                                    <Text style={styles.numbers}>
+                                        {this.state.correctAnswerNumber}
+                                    </Text>
                                     <Image
                                         source={correct}
                                         style={styles.answerImg}
                                     />
-                                    <Text style={styles.numbers}>1</Text>
+                                    <Text style={styles.numbers}>
+                                        {this.state.opponentCorrectAnswerNumber}
+                                    </Text>
                                 </View>
                                 <View style={styles.dividedAnswer}>
-                                    <Text style={styles.numbers}>1</Text>
+                                    <Text style={styles.numbers}>
+                                        {this.state.incorrectAnswerNumber}
+                                    </Text>
                                     <Image
                                         source={incorrect}
                                         style={styles.answerImg}
                                     />
-                                    <Text style={styles.numbers}>1</Text>
+                                    <Text style={styles.numbers}>
+                                        {
+                                            this.state
+                                                .opponentInorrectAnswerNumber
+                                        }
+                                    </Text>
                                 </View>
                                 <View style={styles.dividedAnswer}>
-                                    <Text style={styles.numbers}>1</Text>
+                                    <Text style={styles.numbers}>
+                                        {this.state.unansweredAnswerNumber}
+                                    </Text>
                                     <Image
                                         source={unanswered}
                                         style={styles.answerImg}
                                     />
-                                    <Text style={styles.numbers}>1</Text>
+                                    <Text style={styles.numbers}>
+                                        {
+                                            this.state
+                                                .opponentUnansweredAnswerNumber
+                                        }
+                                    </Text>
                                 </View>
                             </View>
                             <View style={styles.user2Container}>
@@ -69,7 +120,7 @@ class GameStatsScreen extends React.Component {
                                     style={styles.profilePic}
                                 />
                                 <Text style={styles.usernameText}>
-                                    haqotherage
+                                    {this.state.opponentUsername}
                                 </Text>
                             </View>
                         </View>
@@ -79,23 +130,38 @@ class GameStatsScreen extends React.Component {
                                     <Text style={styles.scoresText}>
                                         Oyunu Bitirdin
                                     </Text>
-                                    <Text style={styles.scoresText}>20</Text>
+                                    <Text style={styles.scoresText}>
+                                        {this.state.finishedGamePoint}
+                                    </Text>
                                 </View>
                                 <View style={styles.scoreContainer}>
-                                    <Text style={styles.scoresText}>Doğru Cevap x 3</Text>
-                                    <Text style={styles.scoresText}>60</Text>
+                                    <Text style={styles.scoresText}>
+                                        Doğru Cevap x{' '}
+                                        {this.state.correctAnswerNumber}
+                                    </Text>
+                                    <Text style={styles.scoresText}>
+                                        {this.state.correctAnswerPoint}
+                                    </Text>
                                 </View>
                                 <View style={styles.scoreContainer}>
-                                    <Text style={styles.scoresText}>Kazandın</Text>
-                                    <Text style={styles.scoresText}>100</Text>
+                                    <Text style={styles.scoresText}>
+                                        {this.state.winOrLoseText}
+                                    </Text>
+                                    <Text style={styles.scoresText}>
+                                        {this.state.winOrLosePoint}
+                                    </Text>
                                 </View>
                             </View>
                             <View style={styles.separatorContainer}>
-                            <View style={styles.separatorLine} />
+                                <View style={styles.separatorLine} />
                             </View>
                             <View style={styles.sinaviaScoreContainer}>
-                                <Text style={styles.sinaviaScoreText}>Sınavia Puanı</Text>
-                                <Text style={styles.sinaviaScoreText}>180</Text>
+                                <Text style={styles.sinaviaScoreText}>
+                                    Sınavia Puanı
+                                </Text>
+                                <Text style={styles.sinaviaScoreText}>
+                                    {this.state.totalEarnedPoints}
+                                </Text>
                             </View>
                         </View>
                     </View>
