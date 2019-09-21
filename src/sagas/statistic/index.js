393476b4084114c8f5b848147ae236653120a0c0
Arda Nakisci <anakisci@gmail.com>
2019-09-21 18:03:39 +0300

    delete lastThree months func

--- src/sagas/statistic/index.js
+++ src/sagas/statistic/index.js
@@ -1,6 +1,5 @@
 import { getStatisticsService } from './getStatistics'
 import { getWeeklyStatisticsService } from './getWeeklyStatistics'
-import { getLastThreeMonthsStatisticsService } from './getLastThreeMonthsStatistics'
 import { getLastSixMonthsStatisticsService } from './getLastSixMonthsStatistics'
 import { getMonthlyStatisticsService } from './getMonthlyStatistics'
 
@@ -8,6 +7,5 @@ export const statisticsServices = {
     getStatistics: getStatisticsService,
     getWeeklyStatistics: getWeeklyStatisticsService,
     getMonthlyStatistics: getMonthlyStatisticsService,
-    getLastThreeMonthsStatistics: getLastThreeMonthsStatisticsService,
     getLastSixMonthsStatistics: getLastSixMonthsStatisticsService
 }
