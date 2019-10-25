e330868127718b97c3ae8c440f4902e28fdf123e
Arda Nakisci <anakisci@gmail.com>
2019-10-25 10:46:52 +0300

    change config

--- src/config/index.js
+++ src/config/index.js
@@ -4,10 +4,10 @@ const api = {
     host:
         local === true
             ? 'http://localhost:4000/api/'
-            : 'http://10.253.88.36:4000/api/',
+            : 'http://10.253.12.109:4000/api/',
     hostAWS: 'http://sinavia-test.eu-central-1.elasticbeanstalk.com:4000/api/',
     gameEngine:
-        local === true ? 'http://localhost:5000' : 'http://10.253.88.36:5000',
+        local === true ? 'http://localhost:5000' : 'http://10.253.12.109:5000',
     gameEngineAWS: 'http://sinavia-test.eu-central-1.elasticbeanstalk.com:5000'
 }
 
