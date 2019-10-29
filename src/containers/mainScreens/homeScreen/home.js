fded31a6364fb6af297d6dfcf8b4985cb436ab93
Arda Nakisci <anakisci@gmail.com>
2019-10-29 16:14:06 +0300

    move level finder outside

--- src/containers/mainScreens/homeScreen/home.js
+++ src/containers/mainScreens/homeScreen/home.js
@@ -61,6 +61,8 @@ import {
     navigationReplace
 } from '../../../services/navigationService'
 
+import { levelFinder } from '../../../services/userLevelFinder'
+
 import SWORD from '../../../assets/sword.png'
 const carouselFirstItem = 0
 
@@ -619,7 +621,7 @@ class Home extends React.Component {
                             <Text style={styles.levelText}>
                                 Seviye{' '}
                                 {Math.floor(
-                                    this.levelFinder(
+                                    levelFinder(
                                         this.state.selectedContentTotalPoints
                                     ).level
                                 )}
@@ -630,13 +632,13 @@ class Home extends React.Component {
                                     {
                                         width: wp(
                                             (Math.floor(
-                                                this.levelFinder(
+                                                levelFinder(
                                                     this.state
                                                         .selectedContentTotalPoints
                                                 ).levelProgressScore
                                             ) /
                                                 Math.floor(
-                                                    this.levelFinder(
+                                                    levelFinder(
                                                         this.state
                                                             .selectedContentTotalPoints !==
                                                             0
@@ -653,14 +655,14 @@ class Home extends React.Component {
                             <View style={styles.progressScoreView}>
                                 <Text style={styles.levelInProgressText}>
                                     {Math.floor(
-                                        this.levelFinder(
+                                        levelFinder(
                                             this.state
                                                 .selectedContentTotalPoints
                                         ).levelProgressScore
                                     )}
                                     /
                                     {Math.floor(
-                                        this.levelFinder(
+                                        levelFinder(
                                             this.state
                                                 .selectedContentTotalPoints !==
                                                 0
@@ -1295,39 +1297,6 @@ class Home extends React.Component {
         }
 
         navigationPush(SCENE_KEYS.mainScreens.notifications)
-    }
-
-    // Change score dynamically for every kind of subject
-    levelFinder = userScore => {
-        let level = 1,
-            levelProgressScore = 0,
-            levelProgressLimit = 0
-
-        if (userScore === 0)
-            return { level, levelProgressScore, levelProgressLimit }
-
-        if (userScore < 2000) {
-            level = userScore / 500 + 1
-            levelProgressScore = userScore % 500
-            levelProgressLimit = 500
-        } else if (userScore < 5000) {
-            level = (userScore - 2000) / 750 + 5
-            levelProgressScore = (userScore - 2000) % 750
-            levelProgressLimit = 750
-        } else if (userScore < 11000) {
-            level = (userScore - 5000) / 1000 + 9
-            levelProgressScore = (userScore - 5000) % 1000
-            levelProgressLimit = 1000
-        } else if (userScore < 20000) {
-            level = (userScore - 11000) / 1500 + 15
-            levelProgressScore = (userScore - 11000) % 1500
-            levelProgressLimit = 1500
-        } else {
-            level = (userScore - 20000) / 2000 + 21
-            levelProgressScore = (userScore - 20000) % 2000
-            levelProgressLimit = 2000
-        }
-        return { level, levelProgressScore, levelProgressLimit }
     }
 
     render() {
