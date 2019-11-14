c2a219ed69f9a8cc9e6c708c4e40afdf2fe2b297
Arda Nakışçı <anakisci@gmail.com>
2019-11-14 11:43:56 +0300

    add favourite logic to favouriteScreen (#92)

--- src/containers/mainScreens/favoritesScreen/favorites.js
+++ src/containers/mainScreens/favoritesScreen/favorites.js
@@ -21,24 +21,23 @@ import backButton from '../../../assets/backButton.png'
 import returnLogo from '../../../assets/return.png'
 import shareLogo from '../../../assets/share.png'
 
-import { favouriteQuestion } from '../../../services/apiServices/favouriteQuestion'
-import { fetchUser } from '../../../sagas/user/fetchUser'
+import { clientActions } from '../../../redux/client/actions'
 
-const data = []
-
-// TODO write this file again according to the data from server
 class Favorites extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: data,
+            data: [],
             isModalVisible: false,
             galleryPosition: 1,
             favIconSelected: false,
             // ScrollView item list
             scrollViewList: [],
             startQuestionIndex: 1,
-            correctAnswer: ''
+            correctAnswer: '',
+            // Favoruite variables
+            favouriteIcon: unselectedFav,
+            isFaved: false
         }
     }
 
@@ -166,13 +165,16 @@ class Favorites extends React.Component {
     }
 
     goToIndex(index, questionList) {
-        this.setState({
-            isModalVisible: true,
-            galleryPosition: index + 1,
-            startQuestionIndex: index,
-            data: questionList,
-            correctAnswer: questionList[index].question.correctAnswer
-        })
+        this.setState(
+            {
+                isModalVisible: true,
+                galleryPosition: index + 1,
+                startQuestionIndex: index,
+                data: questionList,
+                correctAnswer: questionList[index].question.correctAnswer
+            },
+            () => this.checkFavouriteStatus()
+        )
     }
 
     // This function takes favouriteQuestions ( it is an array )
@@ -217,6 +219,42 @@ class Favorites extends React.Component {
         return favouriteQuestions
     }
 
+    checkFavouriteStatus = () => {
+        const index = this.props.favouriteQuestions.findIndex(
+            x =>
+                x.questionId ===
+                this.state.data[this.state.galleryPosition - 1].questionId
+        )
+        if (index === -1) {
+            this.setState({
+                favouriteIcon: unselectedFav,
+                isFaved: false
+            })
+        } else {
+            this.setState({ favouriteIcon: selectedFav, isFaved: true })
+        }
+    }
+
+    favouriteOnPress = () => {
+        if (this.state.isFaved) {
+            this.props.unfavouriteQuestion(
+                this.props.clientToken,
+                this.props.clientDBId,
+                this.state.data[this.state.galleryPosition - 1].question,
+                this.props.favouriteQuestions
+            )
+            this.setState({ favouriteIcon: unselectedFav, isFaved: false })
+        } else {
+            this.props.favouriteQuestion(
+                this.props.clientToken,
+                this.props.clientDBId,
+                this.state.data[this.state.galleryPosition - 1].question,
+                this.props.favouriteQuestions
+            )
+            this.setState({ favouriteIcon: selectedFav, isFaved: true })
+        }
+    }
+
     returnButtonOnPress = () => {
         navigationPop()
     }
@@ -248,21 +286,24 @@ class Favorites extends React.Component {
 
     galleryOnScroll = event => {
         this.scrollX = event.nativeEvent.contentOffset.x
-        this.setState({
-            galleryPosition: Math.min(
-                Math.max(
-                    Math.floor(
-                        this.scrollX /
-                            Math.round(Dimensions.get('window').width) +
-                            0.5
-                    ) + 1,
-                    0
+        this.setState(
+            {
+                galleryPosition: Math.min(
+                    Math.max(
+                        Math.floor(
+                            this.scrollX /
+                                Math.round(Dimensions.get('window').width) +
+                                0.5
+                        ) + 1,
+                        0
+                    ),
+                    Object.keys(this.state.data).length /*Image count*/
                 ),
-                Object.keys(this.state.data).length /*Image count*/
-            ),
-            correctAnswer: this.state.data[this.state.galleryPosition - 1]
-                .question.correctAnswer
-        })
+                correctAnswer: this.state.data[this.state.galleryPosition - 1]
+                    .question.correctAnswer
+            },
+            this.checkFavouriteStatus()
+        )
     }
 
     answerSwitcher(buttonNumber) {
@@ -304,9 +345,10 @@ class Favorites extends React.Component {
                     <View style={styles.modalHeader}>
                         <View style={styles.backButtonContainer}>
                             <TouchableOpacity
-                                onPress={() =>
+                                onPress={async () => {
                                     this.setState({ isModalVisible: false })
-                                }
+                                    await this.loadScreen()
+                                }}
                             >
                                 <Image
                                     source={backButton}
@@ -402,24 +444,16 @@ class Favorites extends React.Component {
                             <Text style={styles.answerText}>Doğru cevap</Text>
                         </View>
                         <View style={styles.favIconContainer}>
-                            <TouchableOpacity
-                                onPress={() => {
-                                    this.setState({
-                                        favIconSelected: true
-                                    })
-                                }}
-                            >
+                            <TouchableOpacity onPress={this.favouriteOnPress}>
                                 <Image
-                                    source={
-                                        this.state.favIconSelected === true
-                                            ? selectedFav
-                                            : unselectedFav
-                                    }
+                                    source={this.state.favouriteIcon}
                                     style={styles.favIcon}
                                 />
                             </TouchableOpacity>
                             <Text style={styles.answerText}>
-                                Favoriden çıkar
+                                {this.state.isFaved === true
+                                    ? 'Favoriden çıkar'
+                                    : 'Favoriye ekle'}
                             </Text>
                         </View>
                     </View>
@@ -438,10 +472,31 @@ class Favorites extends React.Component {
 }
 
 const mapStateToProps = state => ({
+    clientToken: state.client.clientToken,
+    clientDBId: state.client.clientDBId,
     favouriteQuestions: state.client.favouriteQuestions,
     gameContentMap: state.gameContent.gameContentMap
 })
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    favouriteQuestion: (clientToken, clientId, question, favedQuestionList) =>
+        dispatch(
+            clientActions.favouriteQuestion(
+                clientToken,
+                clientId,
+                question,
+                favedQuestionList
+            )
+        ),
+    unfavouriteQuestion: (clientToken, clientId, question, favedQuestionList) =>
+        dispatch(
+            clientActions.unfavouriteQuestion(
+                clientToken,
+                clientId,
+                question,
+                favedQuestionList
+            )
+        )
+})
 
-export default connect(mapStateToProps, null)(Favorites)
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites)
