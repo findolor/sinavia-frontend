dd35f5b5a5bc86530ae570a5aa9af8afd83afd36
Arda Nakisci <anakisci@gmail.com>
2019-11-28 12:48:40 +0300

    fix leaderboard

--- src/containers/mainScreens/leaderboardScreen/leaderboard.js
+++ src/containers/mainScreens/leaderboardScreen/leaderboard.js
@@ -58,7 +58,7 @@ class Leaderboard extends React.Component {
             originalGlobalLeaderboardList: [],
             originalFriendsLeaderboardList: [],
             // Last update date
-            hoursPassedSinceUpdate: null
+            hoursPassedSinceUpdate: 1
         }
     }
 
