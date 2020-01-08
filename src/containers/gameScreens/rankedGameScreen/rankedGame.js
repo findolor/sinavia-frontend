faf5c6022f578c4750cf85562511b567bce6f276
Hakan Yılmaz <hakanyilmazz95@gmail.com>
2020-01-08 13:32:41 +0300

    Drawing (#147)
    * add drawing into gamingScreens
    
    * edit group ingame order
    
    * edit jokerReward names
    
    * delete unnecessary import
    
    Co-authored-by: Arda Nakışçı <anakisci@gmail.com>

--- src/containers/gameScreens/rankedGameScreen/rankedGame.js
+++ src/containers/gameScreens/rankedGameScreen/rankedGame.js
@@ -30,6 +30,7 @@ import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-nat
 import AuthButton from '../../../components/authScreen/authButton'
 import * as Animatable from 'react-native-animatable'
 import { interstitialAd } from '../../../services/admobService'
+import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas'
 
 const NORMAL_BUTTON_COLOR = '#C3C3C3'
 const SELECTED_BUTTON_COLOR = '#00d9ef'
@@ -145,7 +146,7 @@ class RankedGame extends React.Component {
         // This is used because with the current colyseus version this is not possible?
         this.checkPingInterval = this.checkPingInterval()
         this.pingInterval = this.pingInterval()
-        
+
         this.props.room.onStateChange.add(state => {
             // We update the UI after state changes
             this.chooseStateAction(state.rankedState)
@@ -458,7 +459,8 @@ class RankedGame extends React.Component {
             case 'show-results':
                 // 8 second countdown time for the results
                 this.setState({
-                    countDownTime: 5
+                    countDownTime: 5,
+                    isQuestionModalVisible: false
                 })
                 this.highlightOpponentButton(
                     rankedState.playerProps[this.state.opponentId].answers[
@@ -1044,25 +1046,54 @@ class RankedGame extends React.Component {
                         animationType={'fade'}
                     >
                         <View style={styles.questionModalContainer}>
-                            <View>
-                                <Image
-                                    source={{
-                                        uri: this.state.questionList[
-                                            this.state.questionNumber
-                                        ]
-                                    }}
-                                    style={styles.questionModalStyle}
-                                />
-                            </View>
-                            <View style={styles.closeModalContainer}>
-                                <TouchableOpacity
-                                    onPress={this.questionModalCloseOnPress}
-                                >
+                            <View style={{ backgroundColor: 'transparent', flex: 1, width: wp(100), justifyContent: 'center'}}>
+                                <View style={{ position: 'absolute', height: hp(78), width: wp(100), justifyContent: 'center'}}>
                                     <Image
-                                        source={ZOOM_OUT_BUTTON}
-                                        style={styles.closeModal}
+                                        source={{
+                                            uri: this.state.questionList[
+                                                this.state.questionNumber
+                                                ]
+                                        }}
+                                        style={styles.questionModalStyle}
                                     />
-                                </TouchableOpacity>
+                                </View>
+                                <RNSketchCanvas
+                                    ref={ref => this.canvas1 = ref}
+                                    containerStyle={{ backgroundColor: 'transparent', flex: 1 }}
+                                    canvasStyle={{ backgroundColor: 'transparent', flex: 1 }}
+                                    onStrokeEnd={data => {
+                                    }}
+                                    closeComponent={<View style={[styles.functionButton, {marginLeft: wp(4)}]}><Text style={{ fontFamily: 'Averta-Bold', color: 'white', fontSize: hp(2.25), textAlign: 'center' }}>Kapat</Text></View>}
+                                    onClosePressed={this.questionModalCloseOnPress}
+                                    undoComponent={<View style={[styles.functionButton, {marginRight: wp(4)}]}><Text style={{ fontFamily: 'Averta-Bold', color: 'white', fontSize: hp(2.25), textAlign: 'center' }}>Geri al</Text></View>}
+                                    onUndoPressed={(id) => {
+                                        this.canvas1.deletePath(id)
+                                    }}
+                                    clearComponent={<View style={[styles.functionButton, {marginRight: wp(4)}]}><Text style={{ fontFamily: 'Averta-Bold', color: 'white', fontSize: hp(2.25), textAlign: 'center' }}>Temizle</Text></View>}
+                                    onClearPressed={() => {
+                                        this.canvas1.clear()
+                                    }}
+                                    eraseComponent={<View style={[styles.functionButton, {marginLeft: wp(4)}]}><Text style={{ fontFamily: 'Averta-Bold', color: 'white', fontSize: hp(2.25), textAlign: 'center' }}>Silgi</Text></View>}
+                                    strokeComponent={color => (
+                                        <View style={[{ backgroundColor: color, borderWidth: hp(1)  }, styles.strokeColorButton]} />
+                                    )}
+                                    strokeSelectedComponent={(color, index, changed) => {
+                                        return (
+                                            <View style={[{ backgroundColor: color}, styles.strokeSelectedColorButton]} />
+                                        )
+                                    }}
+                                    strokeWidthComponent={(w) => {
+                                        return (<View style={styles.strokeWidthButton}>
+                                                <View style={{
+                                                    backgroundColor: 'white',
+                                                    width: Math.sqrt(w / 3) * 10, height: Math.sqrt(w / 3) * 10, borderRadius: Math.sqrt(w / 3) * 10 / 2
+                                                }} />
+                                            </View>
+                                        )
+                                    }}
+                                    defaultStrokeIndex={0}
+                                    defaultStrokeWidth={5}
+                                />
                             </View>
                         </View>
                     </Modal>
