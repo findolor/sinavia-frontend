d6f83a096b8cd1b75bd6f5706eee0082dca71124
Arda Nakisci <anakisci@gmail.com>
2020-01-28 14:38:21 +0300

    remove from android

--- android/app/src/main/java/com/mobee/sinavia/MainApplication.java
+++ android/app/src/main/java/com/mobee/sinavia/MainApplication.java
@@ -12,12 +12,6 @@ import com.BV.LinearGradient.LinearGradientPackage;
 import cl.json.RNSharePackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.horcrux.svg.SvgPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // <-- Add this line
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage; // <-- Add this line
-import io.invertase.firebase.storage.RNFirebaseStoragePackage; // <-- Add this line
-import io.invertase.firebase.admob.RNFirebaseAdMobPackage; // <-- Add this line
-import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -41,13 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       packages.add(new LottiePackage());
-      //packages.add(new RNFirebasePackage());
-      packages.add(new RNFirebaseMessagingPackage());
-      packages.add(new RNFirebaseNotificationsPackage());
-      packages.add(new RNFirebaseAuthPackage());
       packages.add(new LinearGradientPackage());
-      packages.add(new RNFirebaseStoragePackage());
-      packages.add(new RNFirebaseAdMobPackage());
       return packages;
     }
 
