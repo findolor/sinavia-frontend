fcbc3f1e012cef0baafe0331d0e1b275312a079d
Arda Nakisci <anakisci@gmail.com>
2020-04-21 12:06:32 +0300

    impolement user reporting

---
+++ src/services/apiServices/userReporting/reportUser.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { API_ENDPOINT, APP_VERSION } from '../../../config/index'
+import { renewToken } from '../token/renewToken'
+import { flashMessages } from '../../flashMessageBuilder'
+
+export const reportUser = async (headers, params) => {
+    try {
+        let response = await axios.post(
+            API_ENDPOINT + APP_VERSION + '/report/',
+            params,
+            {
+                headers: headers
+            }
+        )
+        return response.data
+    } catch (err) {
+        if (err.message === 'Network Error') {
+            flashMessages.networkError()
+            throw err
+        }
+        if (err.response.status === 401) {
+            let res = await renewToken()
+            headers.Authorization = 'Bearer ' + res.token
+            response = await axios.post(
+                API_ENDPOINT + APP_VERSION + '/report/',
+                params,
+                {
+                    headers: headers
+                }
+            )
+            return response.data
+        } else return err.response
+    }
+}
